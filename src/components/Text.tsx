0c380cabe8b4a794f6b8e240733f343e2ca9fbad
kajjar <kajjar@utu.fi>
2023-12-18 20:48:29 +0200

    Added popular and recent recipes to home page. Recipe pages now display author name and avatar.

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -10,9 +10,9 @@ const Text = ({ children, header, className }: TextProps) => {
   return (
     <>
       {header ? (
-        <a className={`text-3xl text-bold text-stone-800 m-5 ${className}`}>{children}</a>
+        <a className={`text-3xl text-bold text-stone-800 ${className}`}>{children}</a>
       ) : (
-        <a className={`${className} text-xl text-stone-600`}>{children}</a>
+        <a className={`text-xl text-stone-600 ${className}`}>{children}</a>
       )}
     </>
   );
