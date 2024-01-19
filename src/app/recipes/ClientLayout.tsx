ad5577afec5f3588db2f982c045e8890fd0d6a26
Kaarle Järvinen <kaarle46@gmail.com>
2024-01-19 18:40:26 +0200

    Small changes

---
+++ src/app/recipes/ClientLayout.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import SearchBar from '@/components/SearchBar';
+import Text from '@/components/Text';
+import PageBar from '@/components/PageBar';
+import useRecipes from '@/hooks/useRecipes';
+
+const ClientLayout = ({ children }: { children: React.ReactNode }) => {
+  const { loading, error, count, page, query, setQuery } =
+    useRecipes();
+  return (
+    <div>
+      <div>
+        <SearchBar
+          className="flex flex-row items-center justify-between m-4 text-stone-600"
+          query={query}
+          setQuery={setQuery}
+        />
+
+        <Text className="text-md ml-4 flex justify-center mr-5">
+          Page {page} of {Math.ceil(count / 18)}
+        </Text>
+
+        {children}
+      </div>
+      <PageBar
+        search={query}
+        pages={Math.ceil(count / 18)}
+        currentPage={page}
+      />
+    </div>
+  );
+};
+
+export default ClientLayout;
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,56 +1,21 @@
-'use client';
-import SearchBar from '@/components/SearchBar';
-import Text from '@/components/Text';
-import PageBar from '@/components/PageBar';
-import Loader from '@/components/Loader';
-import useRecipes from '@/hooks/useRecipes';
+import { getRecipes } from '@/prismaClient';
 import RecipeCard from '@/components/RecipeCard';
 
-const Recipes = () => {
-  const { recipes, loading, error, count, page, query, setQuery } =
-    useRecipes();
-
-  if (error)
-    return (
-      <div className="flex flex-col justify-center items-center h-screen">
-        <Text>{error}</Text>
-      </div>
-    );
-  if (loading)
-    return (
-      <div className="flex flex-col justify-center items-center h-screen">
-        <Loader />
-      </div>
-    );
-
+const Recipes = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  const query = searchParams.search as string | undefined;
+  const page = searchParams.page as string | undefined;
+  const { recipes } = await getRecipes(page, query);
   return (
-    <div className="flex flex-col min-h-loose justify-between">
-      <div>
-        <SearchBar
-          className="flex flex-row items-center justify-between m-4 text-stone-600"
-          query={query}
-          setQuery={setQuery}
-        />
-
-        <Text className="text-md ml-4 flex justify-center mr-5">
-          Page {page} of {Math.ceil(count / 18)}
-        </Text>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 m-4">
-          {recipes.map((recipe) => (
-            <RecipeCard key={recipe.id} recipe={recipe} />
-          ))}
-        </div>
-      </div>
-      <PageBar
-        search={query}
-        pages={Math.ceil(count / 18)}
-        currentPage={page}
-      />
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 m-4">
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.id} recipe={recipe} />
+      ))}
     </div>
   );
 };
 
 export default Recipes;
-
-export const runtime = 'edge';
