c9a154fcc5c72dacb07b628da20f7ea438bcd85f
Kaarle Järvinen <117437182+KaarleJ@users.noreply.github.com>
2024-01-19 16:04:43 +0200

    Revert "Small edits."

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -2,20 +2,15 @@
 import '@/styles/globals.css';
 import NavBar from '@/components/NavBar';
 import Paper from './Paper';
+import NextNProgress from 'nextjs-progressbar';
 import { Toaster } from 'react-hot-toast';
 import AuthContext from './AuthContext';
-import { Next13ProgressBar } from 'next13-progressbar';
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AuthContext>
-        <Next13ProgressBar
-          height="4px"
-          color="#0A2FFF"
-          options={{ showSpinner: true }}
-          showOnShallow
-        />
+        <NextNProgress color="#29D" />
         <div className="mx-4 sm:mx-20 md:mx-40 lg:mx-80 min-w-min mt-10">
           <NavBar />
           <Paper className="bg-white p-4 shadow-lg min-h-loose">
