9bb2caeeaf7f7138b96c2b452cd3f7516bc8314c
Kaarle Järvinen <kaarle46@gmail.com>
2024-01-25 00:24:50 +0200

    Small tweaks

--- jest.config.mjs
+++ jest.config.mjs
@@ -17,6 +17,7 @@ const config = {
   },
   testTimeout: 30000,
   globalTeardown: './jest.teardown.ts',
+  testMatch: ['**/*.test.ts'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
