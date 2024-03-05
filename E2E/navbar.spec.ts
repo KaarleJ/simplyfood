a4d8a940738d7f41ecec6b39ff582c36d9eb8ed1
Kaarle Järvinen <kaarle46@gmail.com>
2024-03-05 15:35:49 +0200

    Added test for mobile navigation

--- E2E/navbar.spec.ts
+++ E2E/navbar.spec.ts
@@ -7,25 +7,6 @@ test('NavBar is rendered', async ({ page, isMobile }) => {
   await expect(navbar).toBeVisible();
 });
 
-test('Links are rendered and can be navigated', async ({ page }) => {
-  await page.goto('/');
-
-  const homeLink = page.getByRole('link', { name: 'Home' });
-  const recipesLink = page.getByRole('link', { name: 'Recipes' });
-  const signInLink = page.getByRole('link', { name: 'SignIn' });
-  expect(homeLink).toBeAttached();
-  expect(recipesLink).toBeAttached();
-  expect(signInLink).toBeAttached();
-
-  await signInLink.click();
-  await page.waitForURL('**/api/auth/signin');
-  await page.goto('/');
-  await recipesLink.click();
-  await page.waitForURL('**/recipes');
-  await homeLink.click();
-  await page.waitForURL('**/');
-});
-
 test.describe('Mobile view', () => {
   test('Menu is rendered and desktop navbar is not', async ({
     page,
@@ -38,6 +19,31 @@ test.describe('Mobile view', () => {
 
     await expect(navbar).not.toBeVisible();
     await expect(menu).toBeVisible();
+  });
+
+  test('Menu is expanded and links can be navigated', async ({
+    page,
+    isMobile,
+  }) => {
+    test.skip(!isMobile, 'This test is only for mobile view');
+    await page.goto('/');
+    const menu = page.getByTestId('mobile-navbar');
+    await menu.click();
+
+    await expect(menu.getByText('Home')).toBeVisible();
+    await expect(menu.getByText('Recipes')).toBeVisible();
+    await expect(menu.getByText('SignIn')).toBeVisible();
+
+    await menu.getByText('SignIn').click();
+    await page.waitForURL('**/api/auth/signin');
+    await page.goto('/');
+    await menu.click();
+    await menu.getByText('Recipes').click();
+    await page.waitForURL('**/recipes');
+    await page.goto('/');
+    await menu.click();
+    await menu.getByText('Home').click();
+    await page.waitForURL('**/');
   });
 });
 
@@ -53,5 +59,25 @@ test.describe('Desktop view', () => {
 
     await expect(navbar).toBeVisible();
     await expect(menu).not.toBeVisible();
+  });
+
+  test('Links are rendered and can be navigated', async ({ page, isMobile }) => {
+    test.skip(isMobile, 'This test is only for desktop view');
+    await page.goto('/');
+
+    const homeLink = page.getByRole('link', { name: 'Home' });
+    const recipesLink = page.getByRole('link', { name: 'Recipes' });
+    const signInLink = page.getByRole('link', { name: 'SignIn' });
+    expect(homeLink).toBeAttached();
+    expect(recipesLink).toBeAttached();
+    expect(signInLink).toBeAttached();
+
+    await signInLink.click();
+    await page.waitForURL('**/api/auth/signin');
+    await page.goto('/');
+    await recipesLink.click();
+    await page.waitForURL('**/recipes');
+    await homeLink.click();
+    await page.waitForURL('**/');
   });
 });
